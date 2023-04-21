0c41b804dbf1664f9c48f854710f51a57d252a13
JoannaMoussa <joannamoussa.jm@gmail.com>
2023-04-21 12:06:13 +0200

    Add mobile navigation menu

---
+++ courses_platform/static/courses_platform/components/mobile-nav.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const headerBars = document.getElementById("header-bars");
+    const CloseBtn = document.getElementById("mobile-nav-close-btn");
+    const mobileNav = document.getElementById("mobile-navigation");
+    const mobileNavLinks = document.querySelectorAll(".mobile-nav__link");
+
+    headerBars.addEventListener("click", () => {
+        mobileNav.style.display = "flex";
+        document.body.style.overflowY = 'hidden';
+    })
+    CloseBtn.addEventListener("click", () => {
+        mobileNav.style.display = "none";
+        document.body.style.overflowY = 'auto';
+    })
+    mobileNavLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            mobileNav.style.display = "none";
+            document.body.style.overflowY = 'auto';
+        })
+    })
+})
+
+
+
