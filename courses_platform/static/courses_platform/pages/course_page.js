141805bece3f16936dc1d30a6f3903dfd085918e
JoannaMoussa <joannamoussa.jm@gmail.com>
2023-06-15 16:40:10 +0200

    Update enrolled students table in instructor profile

--- courses_platform/static/courses_platform/pages/course_page.js
+++ courses_platform/static/courses_platform/pages/course_page.js
@@ -1,3 +1,33 @@
 $(document).ready( function () {
-    $('#myTable').DataTable();
+    $('#myTable').DataTable({
+        columns: [
+            {
+                data: "First Name",
+            },
+            {
+                data: "Last Name",
+            },
+            {
+                data: "Status",
+                render: function (data, type) {
+                    if (type === "display") {
+                        if (data === "Passed") {
+                            color = "green";
+                        } else if (data === "Enrolled") {
+                            color = "grey";
+                        }
+                        return '<span style="color:' + color + '">' + data + '</span>';
+                    }
+                    if (type === "sort") {
+                        if (data === "Passed") {
+                            return 1;
+                        } else if (data === "Enrolled") {
+                            return 0;
+                        }
+                    }
+                    return data; // this return is for the filter case
+                }
+            },
+        ]
+    });
 } );
